79bcf9a1cad4e8825c9618782b2febda7eb0c4fa
washiz99 <washiz99@x220.jp>
2020-02-10 23:15:55 +0900

    add vagrantfile and provision.sh

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+
+  # OS
+  config.vm.box = "centos/7"
+
+  # Private IP Address
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  # Synced Folder
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+
+  # Provider-specific configuration
+  config.vm.provider "virtualbox" do |vb|
+    # Memory
+    vb.memory = "2048"
+  end
+
+  # Provisioning
+  config.vm.provision :shell, :path => "provision.sh"
+
+end
---
+++ provision.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Disable SELinux
+setenforce 0
+sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+# Install git
+yum -y remove git
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install git2u yum-utils
+yum-config-manager --disable ius
+
+# Install docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+gpasswd -a vagrant docker
+systemctl enable docker
+systemctl start docker
+
+# Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
