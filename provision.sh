5ba911969712b685a26df6d9286375344ba972b3
washiz99 <washiz99@x220.jp>
2020-02-26 00:36:53 +0900

    Add install nodejs

--- provision.sh
+++ provision.sh
@@ -21,3 +21,7 @@ systemctl start docker
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Install Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum -y install nodejs
