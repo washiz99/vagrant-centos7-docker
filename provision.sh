4239bcf5de50d732eddb53fb95224dcb49a63f68
washiz99 <washiz99@x220.jp>
2020-09-21 13:57:20 +0900

    update composer version

--- provision.sh
+++ provision.sh
@@ -18,7 +18,7 @@ systemctl enable docker
 systemctl start docker
 
 # Install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Install Node.js and npm
